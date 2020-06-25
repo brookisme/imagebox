42a6ae884fafd8d746aafc2a5f987d389b383047
Brookie Guzder-Williams <bguzder-williams@wri.org>
2020-06-25 16:48:16 -0700

    image_kit.config

--- image_kit/config.py
+++ image_kit/config.py
@@ -1,6 +1,34 @@
 import os
-
+import yaml
+#
+# CONSTANTS
+#
+IMAGE_KIT_CONFIG_FILE=f'{os.getcwd()}/image_kit.config.yaml'
 FIRST='first'
 LAST='last'
-BAND_ORDERING=os.environ.get('IMAGE_KIT_BAND_ORDERING',FIRST)
+NOISY=os.environ.get('IMAGE_KIT_NOISE',False)
+
+
+#
+# CONFIG
+#
+try: 
+    with open(IMAGE_KIT_CONFIG_FILE,'rb') as file:
+        _config=yaml.safe_load(file)
+        if NOISY:
+            print(f'IMAGE_KIT: config file loaded ({IMAGE_KIT_CONFIG_FILE})')
+except Exception as e:
+    _config={}
+    if NOISY:
+        print(f'IMAGE_KIT: config file ({IMAGE_KIT_CONFIG_FILE}) not found')
+
+
+BAND_ORDERING=_config.get(
+    'band_ordering',
+    os.environ.get('IMAGE_KIT_BAND_ORDERING',FIRST))
+
+
+#
+# DERIVED
+#
 BANDS_FIRST=BAND_ORDERING==FIRST
