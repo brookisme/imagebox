cd6f91854e69c0e3758e394e3c7b6f62aaa90b6d
Brookie Guzder-Williams <bguzder-williams@wri.org>
2020-07-23 10:36:35 -0700

    IK=>IB [env vars tx]

--- imagebox/config.py
+++ imagebox/config.py
@@ -3,29 +3,29 @@ import yaml
 #
 # CONSTANTS
 #
-IMAGE_KIT_CONFIG_FILE=f'{os.getcwd()}/imagebox.config.yaml'
+IMAGE_BOX_CONFIG_FILE=f'{os.getcwd()}/imagebox.config.yaml'
 FIRST='first'
 LAST='last'
-NOISY=os.environ.get('IMAGE_KIT_NOISE',False)
+NOISY=os.environ.get('IMAGE_BOX_NOISE',False)
 
 
 #
 # CONFIG
 #
 try: 
-    with open(IMAGE_KIT_CONFIG_FILE,'rb') as file:
+    with open(IMAGE_BOX_CONFIG_FILE,'rb') as file:
         _config=yaml.safe_load(file)
         if NOISY:
-            print(f'IMAGE_KIT: config file loaded ({IMAGE_KIT_CONFIG_FILE})')
+            print(f'IMAGE_BOX: config file loaded ({IMAGE_BOX_CONFIG_FILE})')
 except Exception as e:
     _config={}
     if NOISY:
-        print(f'IMAGE_KIT: config file ({IMAGE_KIT_CONFIG_FILE}) not found')
+        print(f'IMAGE_BOX: config file ({IMAGE_BOX_CONFIG_FILE}) not found')
 
 
 BAND_ORDERING=_config.get(
     'band_ordering',
-    os.environ.get('IMAGE_KIT_BAND_ORDERING',FIRST))
+    os.environ.get('IMAGE_BOX_BAND_ORDERING',FIRST))
 
 
 #
