e699165ce4129633ffa0c48a99b3c264923034b6
Brookie Guzder-Williams <bguzder-williams@wri.org>
2020-08-12 14:56:11 -0700

    [pypi 0.0.0.8]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'imagebox',
   packages = ['imagebox'],
-  version = '0.0.0.7',
+  version = '0.0.0.8',
   description = 'ImageBox: python utilities for working with multispectral imagery',
   author = 'Brookie Guzder-Williams',
   author_email = 'brook.williams@gmail.com',
